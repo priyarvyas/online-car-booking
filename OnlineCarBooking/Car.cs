a024f631bb68ffb3961a48a219369a54a3ba9615
priyarvyas <vyaspriya5@gmail.com>
2018-04-30 20:34:50 -0400

    added more cars

--- OnlineCarBooking/Car.cs
+++ OnlineCarBooking/Car.cs
@@ -38,7 +38,8 @@ namespace OnlineCarBooking
                 carList = new Dictionary<int, List<Car>>();
                 carList.Add(2, new List<Car>());
                 carList.Add(4, new List<Car>());
-                carList.Add(6, new List<Car>());
+                carList.Add(5, new List<Car>());
+                carList.Add(7, new List<Car>());
                 carList.Add(8, new List<Car>());
                 carList[2].Add(new Car("Porsche", "718 Boxster", "NXD CFG 567",2));
                 carList[2].Add(new Car("Mazda", "MX-5 Miata", "CDJ PNP 345", 2));
@@ -49,6 +50,9 @@ namespace OnlineCarBooking
                 carList[5].Add(new Car("Dodge Challenger", "T/A 392", "VGH EFT 657", 5));
                 carList[5].Add(new Car("Mercedes-Benz", "AMG CLA 45 Coupe", "WER GTY 134", 5));
                 carList[5].Add(new Car("Subaru", "WRX STI", "GHJ WUY 234", 5));
+                carList[7].Add(new Car("Citroen Grand C4 Picasso", "Citroen Grand C4 Picasso", "OIU HDS 657", 7));
+                carList[7].Add(new Car("Land Rover Discovery", "Land Rover Discovery", "NBJ WTU 134", 7));
+                carList[7].Add(new Car("Volvo XC90", "Volvo XC90", "DJI YTR 234", 7));
                 carList[8].Add(new Car("Mercedes Benz Viano", "MPV", "DFG ERT 678", 8));
                 carList[8].Add(new Car("Volkswagen Caravelle", "Volkswagen Caravelle", "WER BHG 678", 8));
                 carList[8].Add(new Car("Peugeot Expert Tepee", "Peugeot Expert Tepee", "BGH SDF 098", 8));
