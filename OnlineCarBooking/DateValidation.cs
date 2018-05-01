dc48c9f014791be152c7c389bf5f267ed9cccc59
NasinVhora <nasinvhora03@gmail.com>
2018-05-01 16:47:59 -0400

    Xaml_DateValidation_Changes

--- OnlineCarBooking/DateValidation.cs
+++ OnlineCarBooking/DateValidation.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace OnlineCarBooking
 {
-    class DateValidation
+    class DateValidation : ValidationRule
     {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            ValidationResult result = new ValidationResult(true, null);
+            DateTime pickupDate = (DateTime)value;
+            if (pickupDate < DateTime.Now)
+            {
+                result = new ValidationResult(false, "Please select future date");
+            }
+            return result;
+        }
+
     }
 }
--- OnlineCarBooking/MainWindow.xaml.cs
+++ OnlineCarBooking/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,10 +23,14 @@ namespace OnlineCarBooking
     {
         List<string> listTime = new List<string>() { "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM" };
         List<string> listSeatNo = new List<string>() { "2", "4", "5", "7","8" };
-       
+
+        
         public MainWindow()
         {
             InitializeComponent();
+            dpPickupDate.SelectedDate = DateTime.Today;
+            dpDropOffDate.SelectedDate = DateTime.Now.AddDays(1);
+            lblErrorMessage.Content = "";
         }
 
         private void cmbPickupTime_Loaded(object sender, RoutedEventArgs e)
@@ -76,5 +81,189 @@ namespace OnlineCarBooking
                 
             }
         }
+
+        private void txtCustomerFName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtCustomerFName.BorderBrush = Brushes.Black;
+            txtCustomerFName.BorderThickness = new Thickness(1);
+        }
+
+        private void txtPhoneNo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtPhoneNo.BorderBrush = Brushes.Black;
+            txtPhoneNo.BorderThickness = new Thickness(1);
+        }
+
+        private void txtAddress_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtAddress.BorderBrush = Brushes.Black;
+            txtAddress.BorderThickness = new Thickness(1);
+        }
+
+        private void txtCity_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtCity.BorderBrush = Brushes.Black;
+            txtCity.BorderThickness = new Thickness(1);
+        }
+
+        private void txtCustomerLName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtCustomerLName.BorderBrush = Brushes.Black;
+            txtCustomerLName.BorderThickness = new Thickness(1);
+        }
+
+        private void txtPostalCode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtPostalCode.BorderBrush = Brushes.Black;
+            txtPostalCode.BorderThickness = new Thickness(1);
+        }
+
+        private void txtDrivingLicenceNo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtDrivingLicenceNo.BorderBrush = Brushes.Black;
+            txtDrivingLicenceNo.BorderThickness = new Thickness(1);
+        }
+
+        private void btnCarBook_Click(object sender, RoutedEventArgs e)
+        {
+            lblErrorMessage.Content = "";
+            bool result = true;
+            string errorMessage = string.Empty;
+            if (txtCustomerFName.Text.Length == 0)
+            {
+                result = false;
+                txtCustomerFName.BorderBrush = Brushes.Red;
+                txtCustomerFName.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content = "Please enter Customer First Name " + Environment.NewLine;
+            }
+            if (txtCustomerLName.Text.Length == 0)
+            {
+                result = false;
+                txtCustomerLName.BorderBrush = Brushes.Red;
+                txtCustomerLName.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content += "Please enter Customer Last Name " + Environment.NewLine;
+            }
+            if (txtPhoneNo.Text.Length == 0)
+            {
+                result = false;
+                txtPhoneNo.BorderBrush = Brushes.Red;
+                txtPhoneNo.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content += "Please enter Phone Number " + Environment.NewLine;
+            }
+            else if (!CheckPhoneNo(txtPhoneNo.Text))
+            {
+                result = false;
+                lblErrorMessage.Content += "Phone number is not valid " + Environment.NewLine;
+            }
+            if (txtAddress.Text.Length == 0)
+            {
+                result = false;
+                txtAddress.BorderBrush = Brushes.Red;
+                txtAddress.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content += "Please enter Address " + Environment.NewLine;
+            }
+            if (txtCity.Text.Length == 0)
+            {
+                result = false;
+                txtCity.BorderBrush = Brushes.Red;
+                txtCity.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content += "Please enter City " + Environment.NewLine;
+            }
+            if (txtPostalCode.Text.Length == 0)
+            {
+                result = false;
+                txtPostalCode.BorderBrush = Brushes.Red;
+                txtPostalCode.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content += "Please enter Postal Code " + Environment.NewLine;
+            }
+            else if (!IsValidPostalCode(txtPostalCode.Text))
+            {
+                result = false;
+                lblErrorMessage.Content += "Postal Code is not valid " + Environment.NewLine;
+            }
+            if (txtDrivingLicenceNo.Text.Length == 0)
+            {
+                result = false;
+                txtDrivingLicenceNo.BorderBrush = Brushes.Red;
+                txtDrivingLicenceNo.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content += "Please enter Driving Licence No " + Environment.NewLine;
+            }
+            if (cmbSeatNo.Text.Length == 0)
+            {
+                result = false;
+                cmbSeatNo.BorderBrush = Brushes.Red;
+                cmbSeatNo.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content += "Please select Seat No " + Environment.NewLine;
+            }
+            if (cmbPickupTime.Text.Length == 0)
+            {
+                result = false;
+                cmbPickupTime.BorderBrush = Brushes.Red;
+                cmbPickupTime.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content += "Please select pickup time " + Environment.NewLine;
+            }
+            if (cmbDropOffTime.Text.Length == 0)
+            {
+                result = false;
+                cmbDropOffTime.BorderBrush = Brushes.Red;
+                cmbDropOffTime.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content += "Please select drop off time " + Environment.NewLine;
+            }
+            if (listAvailCar.Items.Count == 0)
+            {
+                result = false;
+                listAvailCar.BorderBrush = Brushes.Red;
+                listAvailCar.BorderThickness = new Thickness(5);
+                lblErrorMessage.Content += "Please select available car " + Environment.NewLine;
+            }
+
+        }
+
+        private bool CheckPhoneNo(string phoneNo)
+        {
+            try
+            {
+                ulong result;
+                if (phoneNo.Length == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return ulong.TryParse(phoneNo, out result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        private bool IsValidPostalCode(string postalCode)
+        {
+            try
+            {
+                //Canadian Postal Code in the format of "M3A 1A5"
+                string pattern = "^[ABCEGHJ-NPRSTVXY]{1}[0-9]{1}[ABCEGHJ-NPRSTV-Z]{1}[ ]?[0-9]{1}[ABCEGHJ-NPRSTV-Z]{1}[0-9]{1}$";
+
+                System.Text.RegularExpressions.Regex reg = new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+                if (!(reg.IsMatch(postalCode)))
+                    return false;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+
+
+
+
+
     }
 }
